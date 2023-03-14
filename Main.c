8267fe73352b60fb9354c57b80d9769050b3e2fa
Henrik Valve <henrik.valve@kolumbus.fi>
2023-03-14 17:05:17 +0200

    Added task description and removed arguments.

--- Main.c
+++ Main.c
@@ -1,7 +1,14 @@
+/*
+< TASK description:
+< Find the first prime number with 10 digits, and the first prime
+< number with 11 digits.
+< Once found, multiply them and print out the last 15 digits of the
+< result.
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv) {
+int main() {
     printf("Hello World!");
     return 0;
 }
