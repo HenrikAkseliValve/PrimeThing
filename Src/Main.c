13272c71546aecf4ab19c5ef8440c9e987b66fd8
Henrik Valve <henrik.valve@kolumbus.fi>
2023-03-17 14:55:27 +0200

    Long long change
    It is easier to use long long then uint64_t in the main.
    
    Primality test assumes 64 bits so static_assert if long long is more than
    that.

--- Src/Main.c
+++ Src/Main.c
@@ -8,25 +8,17 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <assert.h>
 #include "Primality.h"
-
-/*
- * Since C formatting does not have nbit format length modifier.
- * Use macro to know which one to use.
- */
-#if __SIZEOF_LONG__==8
-	#define UINT64_T_FMARK "%lu"
-#else
-#define UINT64_T_FMARK "%llu"
-#endif
 
 /*
  * Find the next (or equal) prime number from given ODD number.
  * Just support function that hopefully in-lines.
  */
-static inline uint64_t findNextSmallestPrime(uint64_t oddstart){
+static inline long long findNextSmallestPrime(long long oddstart){
+	static_assert(sizeof(long long)==8,"isPrime function is for 64 bit numbers!");
 	// Jump by two since given number is odd.
-	for(uint64_t test=oddstart;true;test+=2){
+	for(long long test=oddstart;true;test+=2){
 		if(isPrime(test)){
 			return test;
 		}
@@ -38,17 +30,17 @@ int main(){
 
 	// Find smallest 10 digit prime.
 	// Start at 1000000001 because 1000000000 is even.
-	uint64_t digit10prime = findNextSmallestPrime(1000000001ll);
-	printf("Found " UINT64_T_FMARK " as 10 digit prime\n",digit10prime);
+	long long digit10prime = findNextSmallestPrime(1000000001ll);
+	printf("Found %llu as 10 digit prime\n",digit10prime);
 
 	// Find smallest 11 digit prime.
-	uint64_t digit11prime = findNextSmallestPrime(10000000001ll);
-	printf("Found " UINT64_T_FMARK " as 11 digit prime\n",digit11prime);
+	long long digit11prime = findNextSmallestPrime(10000000001ll);
+	printf("Found %llu as 11 digit prime\n",digit11prime);
 	// Multiply.
 
 	// Take out the last 15 digits (meaning
 	// 15 least significant base 10 digits).
-	uint64_t product = digit10prime*digit11prime;
+	long long product = digit10prime*digit11prime;
 
 	// Print the result.
 	printf("Answer %.15llu\n",product%1000000000000000ll);
